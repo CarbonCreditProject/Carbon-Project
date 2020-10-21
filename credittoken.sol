3d16b80f0ef4e7d768944b17968509cb8840c6dd
peternderitu <31569127+peternderitu@users.noreply.github.com>
2020-10-21 23:30:44 +0300

    Update credittoken.sol

--- credittoken.sol
+++ credittoken.sol
@@ -4,13 +4,15 @@ pragma experimental ABIEncoderV2;
 import './carboncredits.sol';
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
 
+//SPDX-License-Identifier: UNLICENSED
 contract CreditToken is CarbonCredits,ERC20{
     uint totalsupply;
     uint supply;
+    uint tokenBurnedCount =0;
     uint8 private _decimals;
     
     constructor() ERC20("Carbon", "C") public {
-        _decimals = 0;
+        _setupDecimals(0);
     }
     mapping(address => uint) public BalanceOf;
     mapping(address => uint) public approvedCredits;
@@ -36,7 +38,7 @@ contract CreditToken is CarbonCredits,ERC20{
         _mint(msg.sender, supply);
         return totalsupply;
     }
-    function transferCredits(address to, uint value) public onlyCreditHolder() {
+    function transferCredits(address to, uint value) public  onlyCreditHolder() {
         _transfer(msg.sender, to, value);
         BalanceOf[msg.sender] = BalanceOf[msg.sender].sub(value);
         BalanceOf[to] = BalanceOf[to].add(value);
@@ -49,9 +51,16 @@ contract CreditToken is CarbonCredits,ERC20{
     function approveBurn(uint carbonTokens, address holder) public onlyVerifier{
         tokensApprovedForBurn[holder]+=carbonTokens;
     }
-    function burnTokens() public onlyOwner() {
+    function burnTokens() public onlyOwner() returns(bool) {
         _burn(msg.sender, tokensApprovedForBurn[msg.sender]);
         totalsupply = totalsupply.sub(tokensApprovedForBurn[msg.sender]);
         BalanceOf[msg.sender] = BalanceOf[msg.sender].sub(tokensApprovedForBurn[msg.sender]);
+        return true;
     }
+    function buyCarbonCredits(uint amount) public payable {
+        uint value = amount * 1;
+        require(msg.value > 0);
+        transferCreditsFrom(address(this),msg.sender, value);
+        }
+
 }
